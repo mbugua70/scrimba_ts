349138eb1244ff3d53d66c88201c15581068b6ed
Code MBU <johnmbugua23code@gmail.com>
2025-02-03 19:53:13 +0300

    working on types literals

---
+++ basics/src/tyliterals.ts
@@ -0,0 +1,5 @@
+let myName = "john"
+// by default typescript will infer the above type as string because it's value is a string
+const myNameOne = "Bob"
+// but here typescript will infer the type to a specific value of the variable and this is called type literals
+// hence you can't resign the const
